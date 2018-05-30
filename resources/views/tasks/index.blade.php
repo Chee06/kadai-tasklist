97ba839fad00a53ea800cf3a5da0f855196263e5
chisato.ito <chisato.ito@rakuten.com>
2018-05-30 01:45:34 +0000

    CRUD messages

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,21 +2,16 @@
 
 @section('content')
 
- @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
- @endforeach
+ 
 
-    <h1>メッセージ一覧</h1>
-
-    @if (count($tasks) > 0)
-        <ul>
-            @foreach ($tasks as $task)
-                <li>{{ $task->content }}</li>
-            @endforeach
-        </ul>
-    @endif
+    <h1>タスク一覧</h1>
     
-     {!! link_to_route('tasks.create', '新規メッセージの投稿') !!}
+    @foreach ($tasks as $task)
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+    @endforeach
+
+    
+     {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
 
 
 @endsection
